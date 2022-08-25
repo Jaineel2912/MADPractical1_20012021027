cbe04c17f483454ec183a4d555c94689b7abe378
Jaineel3105 <jaineel.jp@gmail.com>
2022-08-25 20:40:57 +0530

    Practical1 Completed

--- src/main/kotlin/practical2.kt
+++ src/main/kotlin/practical2.kt
@@ -1,4 +1,15 @@
-fun main()
-{
+fun main(){
+    val p:Int = 5
+    val q = p.toDouble()
+    val r:String = "27"
+    val s = Integer.parseInt(r)
+    val t = r.toInt()
+    var u = r.toDouble()
 
+    println("Integer Value : $p")
+    println("Double Value (From Integer) : $q")
+    println("String Value : $r")
+    println("Integer Value1 (From String) : $s")
+    println("Integer Value1 (From String) : $t")
+    println("Double Value (From String) : $u")
 }
